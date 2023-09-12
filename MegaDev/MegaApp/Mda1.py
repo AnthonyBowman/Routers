610513ce89a064ec55e75bdcff6c491db1564449
AnthonyBowman <brian@sherr.co.uk>
2023-09-12 13:22:57 +0100

    Trying to get it working

--- MegaDev/MegaApp/Mda1.py
+++ MegaDev/MegaApp/Mda1.py
@@ -19,20 +19,29 @@ class SSIDListApp(App):
         self.layout.add_widget(self.ssid_button)
 
         # MQTT setup
-        self.mqtt_client = Client()
+        self.mqtt_client = Client(client_id="Mda1")
+        self.mqtt_client.on_connect = self.on_connect  # Add this line
         self.mqtt_client.on_message = self.on_message
         self.mqtt_client.connect("192.168.17.101", 1883, 60)
         
         # Send "GET-SSIDS" message to request SSID list
-        self.mqtt_client.publish("CommandChannel", "GET-SSIDS,0")
-
-        self.mqtt_client.subscribe("ResponseChannel")
+        self.mqtt_client.publish("Command", "GET-SSIDS,0")
 
         return self.layout
+
+    def on_connect(self, client, userdata, flags, rc):
+        # Callback for MQTT client connection
+        if rc == 0:
+            print("Connected to MQTT broker")
+        else:
+            print(f"Connection failed with code {rc}")
+
+        self.mqtt_client.subscribe("Response",1)
 
     def on_message(self, client, userdata, message):
         # Callback for MQTT message reception
         # Extract and display SSID list received over MQTT
+        test="hello"
         ssids = message.payload.decode("utf-8").split(",")
         for ssid in ssids:
             btn = Button(text=ssid, size_hint_y=None, height=44)
